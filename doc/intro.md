5077d4c98676f79a9391def12980a43bee7705b2
Christian Weilbach <christian@topiq.es>
2016-12-09 16:22:06 +0100

    Fiat lux.#

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to chat42
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/chat42/core_test.clj
@@ -0,0 +1,7 @@
+(ns chat42.core-test
+  (:require [clojure.test :refer :all]
+            [chat42.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
