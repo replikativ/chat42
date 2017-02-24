5ea26a8c93b48ceac6188ce42a6b69469a45e535
Christian Weilbach <christian@topiq.es>
2017-02-24 20:14:22 +0100

    Cleanup code.

--- src/clj/chat42/core.clj
+++ src/clj/chat42/core.clj
@@ -1,77 +1,23 @@
 (ns chat42.core
-  (:gen-class :main true)
   (:require [hasch.core :refer [uuid]]
-            [replikativ.crdt.ormap.realize :refer [stream-into-identity!]]
-            [replikativ.crdt.ormap.stage :as s]
-            [replikativ.stage :refer [create-stage! connect! subscribe-crdts!]]
-            [replikativ.peer :refer [client-peer server-peer]]
+            [replikativ.peer :refer [server-peer]]
 
             [kabel.peer :refer [start stop]]
             [konserve.memory :refer [new-mem-store]]
-            [konserve.filestore :refer [new-fs-store delete-store]]
 
-            [superv.async :refer [<?? <? S go-try go-loop-try]] ;; core.async error handling
-            [clojure.core.async :refer [chan go-loop go] :as async]))
+            [superv.async :refer [<?? S]] ;; core.async error handling
+            [clojure.core.async :refer [chan] :as async]))
 
-(def user "mail:alice@replikativ.io")
-(def ormap-id #uuid "7d274663-9396-4247-910b-409ae35fe98d")
 (def uri "ws://127.0.0.1:31744")
-(def val-atom (atom {}))
-(def store-path "/tmp/chat42-store")
-(defonce server-state (atom nil))
-
-(def stream-eval-fns
-  {'(fn [_ new] [new]) (fn [a new]
-                        (reset! a {(uuid new) new})
-                        a)
-   'assoc (fn [a new]
-            (swap! a assoc (uuid new) new)
-            a)
-   'dissoc (fn [a new]
-             (swap! a dissoc (uuid new))
-             a)})
-
-(defn start-all-services []
-  (let [store (<?? S (new-mem-store))
-        peer (<?? S (server-peer S store uri))
-        _ (<?? S (start peer))
-        stage (<?? S (create-stage! user peer))
-        stream (stream-into-identity! stage [user ormap-id] stream-eval-fns val-atom)]
-    ;; just to print messages to STDOUT
-    (add-watch val-atom :messages
-               (fn [_ _ _ val]
-                 (let [{:keys [date name text]} (-> val vals last)]
-                   (println (str "Date: " (.toString (java.util.Date. date)) " name: " name " text: " text)))))
-    (<?? S (s/create-ormap! stage :description "messages" :id ormap-id))
-    (println "Chat42 replikativ server peer up and running!")
-    
-    {:store store
-     :peer peer
-     :stream stream
-     :stage stage}))
 
 (defn -main [& args]
-  (let [c (chan)]
-    (reset! server-state (start-all-services))
-    (<?? S c)))
+  (let [store (<?? S (new-mem-store))
+        peer (<?? S (server-peer S store uri))]
+    (<?? S (start peer))
+    (println "Chat42 replikativ server peer up and running!" uri)
+    ;; HACK blocking main termination
+    (<?? S (chan))))
 
-(comment
 
-  (def state (start-all-services))
-  
-  @val-atom
-  
-  (async/close! (-> state :stream :close-ch))
-
-  (<?? S (stop (:peer state)))
-
-  (reset! val-atom nil)
-
-  (def test-msg {:text "Hi" :name "Yo" :date (.getTime (java.util.Date.))})
-
-  (<?? S (s/assoc! (:stage state) [user ormap-id] (uuid test-msg) [['assoc test-msg]]))
- 
-
-  )
 
 
--- src/cljs/chat42/core.cljs
+++ src/cljs/chat42/core.cljs
@@ -1,4 +1,3 @@
-;; clean namespace system, compatible with Google Closure minification
 (ns chat42.core
   (:require [konserve.memory :refer [new-mem-store]]
             [replikativ.peer :refer [client-peer]]
@@ -21,10 +20,57 @@
   (:require-macros [superv.async :refer [go-try <? go-loop-try]]
                    [cljs.core.async.macros :refer [go-loop]]))
 
+;; 1. app constants
+(def user "mail:alice@replikativ.io")
+(def ormap-id #uuid "7d274663-9396-4247-910b-409ae35fe98d")
+(def uri "ws://127.0.0.1:31744")
+
+
 (enable-console-print!)
 
 ;; Have a look at the replikativ "Get started" tutorial to understand how the
 ;; replikativ parts work: http://replikativ.io/tut/get-started.html
+
+(def stream-eval-fns
+  {'assoc (fn [a new]
+            (swap! a assoc (uuid new) new)
+            a)
+   'dissoc (fn [a new]
+             (swap! a dissoc (uuid new))
+             a)})
+
+
+(defonce val-atom (atom {}))
+
+
+(defn setup-replikativ []
+  (go-try S
+    (let [local-store (<? S (new-mem-store))
+          local-peer (<? S (client-peer S local-store))
+          stage (<? S (create-stage! user local-peer))
+          stream (stream-into-identity! stage
+                                        [user ormap-id]
+                                        stream-eval-fns
+                                        val-atom)]
+      (<? S (s/create-ormap! stage
+                             :description "messages"
+                             :id ormap-id))
+      (connect! stage uri)
+      {:store local-store
+       :stage stage
+       :stream stream
+       :peer local-peer})))
+
+(declare client-state)
+;; this is the only state changing function
+(defn send-message! [app-state msg]
+  (s/assoc! (:stage client-state)
+            [user ormap-id]
+            (uuid msg)
+            [['assoc msg]]))
+
+
+
 
 ;; helper functions
 (defn format-time [d]
@@ -38,61 +84,12 @@
       (>= secs 0) (str  " seconds ago"))))
 
 
-;; replikativ setup
-(def user "mail:alice@replikativ.io")
-(def ormap-id #uuid "7d274663-9396-4247-910b-409ae35fe98d")
-(def uri "ws://127.0.0.1:31744")
-
-(def stream-eval-fns
-  {'(fn [_ new] [new]) (fn [a new]
-                        (reset! a {(uuid new) new})
-                        a)
-   'assoc (fn [a new]
-            (swap! a assoc (uuid new) new)
-            a)
-   'dissoc (fn [a new]
-             (swap! a dissoc (uuid new))
-             a)})
-
-(defn start-local []
-  (go-try S
-   (let [local-store (<? S (new-mem-store))
-         local-peer (<? S (client-peer S local-store))
-         stage (<? S (create-stage! user local-peer))]
-     {:store local-store
-      :stage stage
-      :peer local-peer})))
-
-
-(defonce val-atom (atom {}))
-
-
-(defn init []
-  (go-try S
-   (def client-state (<? S (start-local)))
-
-   (stream-into-identity! (:stage client-state)
-                          [user ormap-id]
-                          stream-eval-fns
-                          val-atom)
-   (<? S (s/create-ormap! (:stage client-state)
-                          :description "messages"
-                          :id ormap-id))
-   (<? S (connect! (:stage client-state) uri))))
-
-
 ;; Material UI with Om
-
 (defn create-msg [name text]
   {:text text
    :name name
    :date (.getTime (js/Date.))})
 
-
-;; this is the only state changing function
-(defn send-message! [app-state msg]
-  (go-try S (<? S (s/assoc! (:stage client-state)
-                            [user ormap-id] (uuid msg) [['assoc msg]]))))
 
 (defn target-val [e]
   (.. e -target -value))
@@ -103,8 +100,7 @@
            (ui/text-field
             {:floating-label-text "Name"
              :class-name "w-80"
-             :on-change #(om/update-state! comp
-                                           assoc :input-name (target-val %))
+             :on-change #(om/update-state! comp assoc :input-name (target-val %))
              :value input-name})))
 
 
@@ -126,6 +122,7 @@
                                  (om/update-state! comp assoc :input-text "")))
                              :value input-text}))))
 
+
 (defn send-button [comp input-text input-name]
   (let [app-state (om/props comp)]
     (dom/div #js {:className "center-xs"}
@@ -145,16 +142,16 @@
                  :key (uuid (str date))}))
 
 
-;; 6. React App
+;; React App
 (defui App
   Object
   (componentWillMount [this]
-                     (om/set-state!
-                      this
-                      {:input-name ""
-                       :input-text ""
-                       :snackbar {:message "hello"
-                                  :open false}}))
+    (om/set-state!
+     this
+     {:input-name ""
+      :input-text ""
+      :snackbar {:message "hello"
+                 :open false}}))
   (render [this]
     (let [app-state (om/props this)
           {:keys [input-name input-text snackbar]} (om/get-state this)]
@@ -179,7 +176,9 @@
   (om/reconciler {:state val-atom}))
 
 (defn main [& args]
-  (init))
+  (go-try S
+          (def client-state (<? S (setup-replikativ)))
+          (.error js/console "INITED")))
 
 ;; for figwheel not in main
 (om/add-root! reconciler App (.getElementById js/document "app"))
